b54fc435e87f21483926ec6d07d354757b54fbaa
Noah Roberts <noahroberts@Noahs-MacBook-Pro.local>
2021-06-28 16:37:14 -0500

    tests added

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DATABASE_URL,
-  "ssl": { rejectUnauthorized: false }
+  "connectionString": /*(process.env.NODE_ENV === 'test')
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL,*/
+  "postgresql://postgres@localhost/recipes-test"
+  //"ssl": { rejectUnauthorized: false }
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,101 @@
 const app = require('../src/app')
+const knex = require('knex')
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+describe('Recipes API:', () => {
+  let db;
+  let recipes = [
+    {
+      "title": "recipe 1",
+      "abstract": "asdf",
+      "coffee": 1,
+      "grind": "fine",
+      "water": 100, 
+      "method": "lorem ipsum"
+    },
+    {
+      "title": "recipe 2",
+      "abstract": "asdf",
+      "coffee": 2,
+      "grind": "medium",
+      "water": 200, 
+      "method": "lorem ipsum"
+    },
+    {
+      "title": "recipe 3",
+      "abstract": "asdf",
+      "coffee": 3,
+      "grind": "coarse",
+      "water": 300, 
+      "method": "lorem ipsum"
+    },
+  ]
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DATABASE_URL
+    })
+    app.set('db', db)
+  });
+
+  before('cleanup', () => db.raw('TRUNCATE TABLE recipes RESTART IDENTITY;'));
+
+  afterEach('cleanup', () => db.raw('TRUNCATE TABLE recipes RESTART IDENTITY;')); 
+
+  after('disconnect from the database', () => db.destroy());
+
+  describe('App', () => {
+    it('GET / responds with 200 containing "Hello, world!"', () => {
+      return supertest(app)
+        .get('/')
+        .expect(200, 'Hello, world!')
+    })
+  })
+
+  describe('GET /recipes', () => {
+    
+    beforeEach('insert some recipes', () => {
+      return db('recipes').insert(recipes);
+    })
+
+    it('should respond to GET `/recipes` with an array of recipes and status 200', () => {
+      return supertest(app)
+        .get('/recipes')
+        .expect(200)
+        .expect(res => {
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(recipes.length);
+          res.body.forEach((recipe) => {
+            expect(recipe).to.be.a('object');
+            expect(recipe).to.include.keys('id', 'title', 'abstract', 'coffee', 'grind', 'water', 'method');
+          });
+        });
+    });
+  });
+
+  describe('POST /recipes', () => {
+
+    it('should create and return a new recipe when provided valid data', () => {
+      const newRecipe = {
+        "title": "recipe 1",
+        "abstract": "asdf",
+        "coffee": 1,
+        "grind": "fine",
+        "water": 100, 
+        "method": "lorem ipsum"
+      };
+
+      return supertest(app)
+        .post('/recipes')
+        .send(newRecipe)
+        .expect(201)
+        .expect( res => {
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.include.keys('id', 'title', 'abstract', 'coffee', 'grind', 'water', 'method', 'link');
+          expect(res.body.title).to.equal(newRecipe.title);
+          expect(res.headers.location).to.equal(`/recipes/${res.body.id}`)
+        })
+    })
   })
 })
+ 
