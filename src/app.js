70ed17491f100247cfeabf4e2d8de496ff613594
Noah Roberts <noahroberts@Noahs-MacBook-Pro.local>
2021-06-28 20:42:17 -0500

    modularized error handler

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const errorHandler = require('./error-handler')
 const recipesRouter = require('./recipes/recipes-router')
 
 const app = express()
@@ -18,13 +19,7 @@ app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: error.message, error }
-  }
-  res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app 
 
